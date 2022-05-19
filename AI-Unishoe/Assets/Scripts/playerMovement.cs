e7aaad0de3c55cdfd09c01a746436444396e2b1f
Wuggie <wuggie88@yahoo.co.uk>
2022-05-19 10:04:45 +0200

    Movement updated, enemy speed and death trigger

--- AI-Unishoe/Assets/Scripts/playerMovement.cs
+++ AI-Unishoe/Assets/Scripts/playerMovement.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerMovement : MonoBehaviour
 {
-
+    public float deathDistance = 0f;
     public CharacterController2D controller;
 
     float horizontalMove = 0f;
@@ -43,5 +44,9 @@ public class playerMovement : MonoBehaviour
         //move character
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
+            
+        if(this.transform.position.y <= deathDistance) {
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 }
