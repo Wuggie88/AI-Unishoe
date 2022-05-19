006a0369fccde7ee99b1bc23a9da6bff01fb5059
Wuggie <wuggie88@yahoo.co.uk>
2022-05-19 13:37:52 +0200

    Win condition!! and graphics for unicorn and nests

---
+++ AI-Unishoe/Assets/Scripts/WinZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinZone : MonoBehaviour
+{
+
+    public Text winningText;
+    public GameObject[] enemies;
+
+    private void Start()
+    {
+        winningText = GameObject.Find("WinningText").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 8) {
+            winningText.text = "VICTORY!!";
+
+            enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+            foreach(GameObject Enemy in enemies) {
+                Enemy.gameObject.SetActive(false);
+            }
+        }
+    }   
+}
